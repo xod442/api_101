f4b4ea82f69bf9ec5d340f5c9b0e8d2ed4f2cd7c
Rick Kauffman <rick@rickkauffman.com>
2022-12-06 13:00:19 -0700

    init4

--- answers/app.py
+++ answers/app.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
 # encoding: utf-8
 '''
-Stuff here:
+ ________  ________  ___           _____  ________    _____     
+|\   __  \|\   __  \|\  \         / __  \|\   __  \  / __  \
+\ \  \|\  \ \  \|\  \ \  \       |\/_|\  \ \  \|\  \|\/_|\  \
+ \ \   __  \ \   ____\ \  \      \|/ \ \  \ \  \\\  \|/ \ \  \
+  \ \  \ \  \ \  \___|\ \  \          \ \  \ \  \\\  \   \ \  \
+   \ \__\ \__\ \__\    \ \__\          \ \__\ \_______\   \ \__\
+    \|__|\|__|\|__|     \|__|           \|__|\|_______|    \|__|
+
+
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0.
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+__author__ = "@"
+__credits__ = ["You Name"]
+__license__ = "Apache2"
+__version__ = "0.1.1"
+__maintainer__ = "Your Name"
+__status__ = "Alpha"
+
 '''
 import json
 from flask import Flask
